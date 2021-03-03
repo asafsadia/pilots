e0999e96f8a31da578803d84beb6b33f8f322cad
asafsadia <asafsadia400@gmail.com>
2021-03-03 16:17:49 +0200

    two

--- src/test/java/pageObject/BasePage.java
+++ src/test/java/pageObject/BasePage.java
@@ -10,6 +10,9 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /*Base class that wraps Selenium operations and will be inherited by all POMs*/
+/*Base class that wraps Selenium operations and will be inherited by all POMs*/
+/*Base class that wraps Selenium operations and will be inherited by all POMs*/
+/*Base class that wraps Selenium operations and will be inherited by all POMs*/
 public class BasePage {
 	WebDriver driver;
 	WebDriverWait wait;
